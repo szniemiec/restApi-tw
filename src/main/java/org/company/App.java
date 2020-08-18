b4f036f91e2891343aaf223b205e0f537258646e
szniemiec <szniemiec@yahoo.com>
2020-08-18 11:36:26 +0200

    Create PokemonDao class with loadAll() and get() methods

--- src/main/java/org/company/App.java
+++ src/main/java/org/company/App.java
@@ -1,10 +1,6 @@
 package org.company;
 
-import org.company.database.PostgreSQLJDBC;
-
 public class App {
-
     public static void main(String[] args) {
-        PostgreSQLJDBC database = new PostgreSQLJDBC();
     }
 }
